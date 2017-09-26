3e0b23f6a74468e9d17f6b2f8d56cd7277a55d4a
Belkisa <kisacausevic@gmail.com>
2017-09-26 16:54:55 -0500

    Error messages working, fields are not staying after error occurs

--- main.py
+++ main.py
@@ -10,20 +10,48 @@ def index():
 @app.route('/', methods=['POST'])
 def validate_info():
     username = request.form['username']
+    password = request.form['password']
+    password2 = request.form['verify-password']
+    email = request.form['email']
 
-    if len(username) < 3 or len(username) > 20:
-        error = "That's not a valid username"
-        return render_template('index.html', username_error=error)
+    username_error = ''
+    password_error = ''
+    verify_error = ''
+    email_error = ''
 
-    #password = request.form['password']
-    #verify_password = request.form['verify-password']
 
-    #if verify_password != password:
-    #    error = "Passwords do not match"
-    #    return redirect('/?error=' + error)
+    if len(username) < 3 or len(username) > 20 or username == '':
+        username_error = "That's not a valid username"
+        username = ''
+    else:
+        username = username
+
+    if len(password) < 3 or len(password) > 20 or password == '' or ' ' in password:
+        password_error = "That's not a valid password"
+
+    if password2 != password:
+        verify_error = "Passwords don't match"
+    
+    if email != '':
+        if '@' not in email or '.' not in email:
+            email_error = "Please enter a valid email address"
+            email = ''
+        else:
+            email = email
+    
+    if not username_error and not password_error and not verify_error and not email_error:
+        return redirect('/welcome?username={0}'.format(username))
+    else:
+        return render_template('index.html',username_error=username_error,
+            password_error=password_error,verify_error=verify_error,email_error=email_error,
+            username=username,email=email)
+
+
+
 
 @app.route('/welcome')
 def success():
+    username = request.args.get('username')
     return render_template('welcome.html', username=username)
 
 app.run()
