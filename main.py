7d18182fee0013b0db5e0676acee1c6320ba2fed
Belkisa <kisacausevic@gmail.com>
2017-09-26 16:12:39 -0500

    Added welcome page, username error

--- main.py
+++ main.py
@@ -7,5 +7,24 @@ app.config['DEBUG'] = True
 def index():
     return render_template('index.html')
 
+@app.route('/', methods=['POST'])
+def validate_info():
+    username = request.form['username']
+
+    if len(username) < 3 or len(username) > 20:
+        error = "That's not a valid username"
+        return render_template('index.html', username_error=error)
+
+    #password = request.form['password']
+    #verify_password = request.form['verify-password']
+
+    #if verify_password != password:
+    #    error = "Passwords do not match"
+    #    return redirect('/?error=' + error)
+
+@app.route('/welcome')
+def success():
+    return render_template('welcome.html', username=username)
+
 app.run()
 
